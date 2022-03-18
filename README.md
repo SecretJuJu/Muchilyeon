ac098a4a0d49b0cc3c12cc169c10ef8da4ce585e
SecretJuJu <dev.tmdqh@gmail.com>
2022-03-18 19:27:07 +0900

    fix: webpack build bug fix

--- README.md
+++ README.md
@@ -1,18 +1,2 @@
 <h1 align="center">MUYAHO</h1>
 <p align="center">muyaho on every click with sound</p>
-
-<h3 align="center">
-  <a href="https://jhaemin.github.io/muyaho">Live Demo →</a>
-</h3>
-
-<p align="center">
-  <img src="https://user-images.githubusercontent.com/19797697/124208282-2f11d180-db22-11eb-8862-b164b1367da6.gif" alt="muyaho preview" />
-</p>
-
-```zsh
-> npm install muyaho
-```
-
-```js
-import 'muyaho'
-```
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,22 @@
 /** @type {import('webpack/types').Configuration} */
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 const config = {
-  entry: __dirname + '/src/index.js',
-  mode: 'production',
+  entry: __dirname + "/src/index.js",
+  mode: "production",
   output: {
-    path: __dirname + '/build',
-    filename: 'index.js',
+    path: __dirname + "/build",
+    filename: "index.js",
   },
   module: {
     rules: [
       {
-        test: /\.(png|mp3)$/i,
-        type: 'asset/inline'
+        test: /\.(gif|mp3)$/i,
+        type: "asset/inline",
       },
     ],
   },
-}
+  plugins: [new HtmlWebpackPlugin()],
+};
 
-module.exports = config
+module.exports = config;
